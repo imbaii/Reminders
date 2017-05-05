899296f098479f9eb9f9054643a8071878b17f32
Bob <imbaii@163.com>
2017-05-05 18:54:38 +0800

    添加示例事项

--- app/src/main/java/com/example/reminders/RemindersActivity.java
+++ app/src/main/java/com/example/reminders/RemindersActivity.java
@@ -1,13 +1,11 @@
 package com.example.reminders;
 
 import android.database.Cursor;
-import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class RemindersActivity extends AppCompatActivity {
@@ -29,13 +27,19 @@ public class RemindersActivity extends AppCompatActivity {
         mDbAdapter = new RemindersDbAdapter(this);
         mDbAdapter.open();
 
+        if (savedInstanceState == null) {
+            //Clear add data
+            mDbAdapter.deleteAllReminders();
+            //Add some data
+            insertSomeReminders();
+        }
+
         Cursor cursor = mDbAdapter.fetchAllReminders();
         //from columns defined in the db
         String[] from = new String[]{
                 RemindersDbAdapter.COL_CONTENT};
         //to the ids of views in the layout
         int[] to = new int[]{R.id.row_text};
-
         mCursorAdapter = new RemindersSimpleCursorAdapter(
                 //context
                 RemindersActivity.this,
@@ -54,6 +58,25 @@ public class RemindersActivity extends AppCompatActivity {
         mListView.setAdapter(mCursorAdapter);
     }
 
+    private void insertSomeReminders() {
+        mDbAdapter.createReminder("Buy Learn Android Studio", true);
+        mDbAdapter.createReminder("Send Dad birthday gift", false);
+        mDbAdapter.createReminder("Dinner at the Gage on Friday", false);
+        mDbAdapter.createReminder("String squash racket", false);
+        mDbAdapter.createReminder("Shovel and salt walkways", false);
+        mDbAdapter.createReminder("Prepare Advanced Android syllabus", true);
+        mDbAdapter.createReminder("Buy new office chair", false);
+        mDbAdapter.createReminder("Call Auto-body shop for quote", false);
+        mDbAdapter.createReminder("Renew membership to club", false);
+        mDbAdapter.createReminder("Buy new Galaxy Android phone", true);
+        mDbAdapter.createReminder("Sell old Android phone - auction", false);
+        mDbAdapter.createReminder("Buy new paddles for kayaks", false);
+        mDbAdapter.createReminder("Call accountant about tax returns", false);
+        mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
+        mDbAdapter.createReminder("Call the Dalai Lama back", true);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_reminders, menu);
         return true;
